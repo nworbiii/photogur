9f291ad0bdd9826347a3b083e4fb6bd4af791f2b
Kyle Brown <kylealbrown@gmail.com>
2015-01-29 11:31:04 -0500

    finish setting up initial oauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :pictures => { omniauth_callbacks: 'omniauth_callbacks' }
+  match '/users/:id/finish_signup' => 'users#finish_signup',
+    via: [:get, :patch], :as => :finish_signup
+
+
+
   resources :pictures
 
   root to: 'pictures#index'
